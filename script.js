11f78bed10849d9038dc96b99ec5618dc70a4301
Nitin <kumarnitinrajput238@gmail.com>
2026-02-21 23:02:54 +0530

    Initial project setup

---
+++ script.js
@@ -0,0 +1,38 @@
+let display = document.getElementById("display")
+function Apendvalue(value) {
+    display.value += value;
+}
+function ClearDisplay() {
+    display.value = "";
+}
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "error";
+    }
+}
+
+document.addEventListener("keydown", function (event) {
+    let key = event.key;
+    // Numebers
+    if (!isNaN(key)) {
+        Apendvalue(key);
+    }
+    //operators
+    else if (key == '+' || key == '-' || key == '*' || key == '/') {
+        Apendvalue(key);
+    }
+    //Decimal
+    else if (key == '.') {
+        Apendvalue(key);
+    }
+    //Calculate=Enter
+    else if (key == "Enter") {
+        calculate()
+    }
+    //Clr Dispaly=Escape
+    else if (key == "Escape") {
+        ClearDisplay();
+    }
+});
